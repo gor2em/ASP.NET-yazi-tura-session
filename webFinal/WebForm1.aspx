c83acb5a8fadc4510c4190675235b3dc5c149e35
gor2em <grkem.squash99@gmail.com>
2020-06-06 15:34:23 +0300

    C# ASP.NET Session ile yazı tura uygulaması

---
+++ webFinal/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="webFinal.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        #main{
+            width:1200px;
+            margin:20px auto;
+            text-align:center;
+        }
+        .yt{
+            font-size:150px;
+            color:rebeccapurple;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="main">
+            <asp:Button ID="Button1" runat="server" Text="Şansını dene!" OnClick="Button1_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lbl_YT" runat="server" Text="" CssClass="yt"></asp:Label>
+
+            <br /><br />
+            <asp:Label ID="lbl_yazi" runat="server"></asp:Label>
+            <asp:Label ID="lbl_tura" runat="server"></asp:Label>
+            <asp:Label ID="lbl_top" runat="server"></asp:Label>
+
+             
+            <asp:Label ID="lbl_deger" runat="server" Text=""></asp:Label>
+
+            <br /><br />
+      
+        </div>
+    </form>
+</body>
+</html>
---
+++ webFinal/WebForm1.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace webFinal
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        object toplam, yaziToplam, turaToplam, ytSonuc;
+        private static int yazi, tura, top;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            toplam = Session["toplamSonuc"];
+            yaziToplam = Session["yaziSonuc"];
+            turaToplam = Session["turaSonuc"];
+            ytSonuc = Session["ytSonuc"];
+            if (toplam == null || yaziToplam == null || turaToplam == null || ytSonuc == null) lbl_deger.Visible = false;
+            else
+            {
+                lbl_deger.Visible = true;
+                lbl_YT.Text = ytSonuc.ToString();
+                lbl_deger.Text = "Yazı: " + yaziToplam + " Tura: " + turaToplam + " Toplam: " + toplam;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            yazi = Convert.ToInt32(yaziToplam);
+            tura = Convert.ToInt32(turaToplam);
+            top = Convert.ToInt32(toplam);
+            Random rnd = new Random();
+            int rastgele = rnd.Next(0, 2);
+
+            if (rastgele == 0)
+            {
+                yazi++;
+                top++;
+                lbl_YT.Text = "YAZI";
+            }
+            else
+            {
+                tura++;
+                top++;
+                lbl_YT.Text = "TURA";
+            }
+
+            lbl_yazi.Text = yazi.ToString();
+            lbl_tura.Text = tura.ToString();
+            lbl_top.Text = top.ToString();
+
+
+            Session.Add("toplamSonuc", lbl_top.Text);
+            Session.Add("yaziSonuc", lbl_yazi.Text);
+            Session.Add("turaSonuc", lbl_tura.Text);
+            Session.Add("ytSonuc", lbl_YT.Text);
+
+            Response.Redirect("/WebForm1.aspx");
+
+        }
+    }
+}
